d34e3b028384040a1f9bdf7f0609e9ece65b668b
Promeroguaq <122805374+Promeroguaq@users.noreply.github.com>
2023-03-07 14:15:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-solid fa-chevron-down') {
+            icon.className = "fa-solid fa-chevron-up"
+        } else { 
+            icon.className = "fa-solid fa-chevron-down";
+        }
+    })
+})
